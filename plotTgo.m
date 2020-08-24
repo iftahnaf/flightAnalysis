493c58d6c4e348b5307b1e7a76a0d9878147eb81
iftahnaf <iftach@campus.technion.ac.il>
2020-08-24 09:17:44 +0300

    imrpving GUI

--- plotTgo.m
+++ plotTgo.m
@@ -26,6 +26,10 @@ V = zeros(1,length(x));
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+Range = zeros(1,length(x));
+>>>>>>> devel
 =======
 Range = zeros(1,length(x));
 >>>>>>> devel
@@ -46,12 +50,15 @@ for i = 1 : length(x)
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     Tgo(i) = tgo_interception(R(i,:),V(i,:),drho,m,minTgo);
 end
 
 figure(1)
 plot(time_ref-time_ref(1),Tgo,'k','linewidth',1);
 =======
+=======
+>>>>>>> devel
 =======
 >>>>>>> devel
 =======
@@ -70,6 +77,9 @@ figure(1)
 plot(time_ref(1:k)-time_ref(1),Tgo(1:k),'k','linewidth',1);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> devel
+=======
 >>>>>>> devel
 =======
 >>>>>>> devel
