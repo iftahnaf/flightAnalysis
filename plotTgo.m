01ea616bf1f668144301d320aebb3e16071827f9
iftahnaf <iftach@campus.technion.ac.il>
2020-08-24 09:13:51 +0300

    improving range and tgo plots

--- plotTgo.m
+++ plotTgo.m
@@ -23,6 +23,7 @@ drho = rho_u - rho_w;
 Tgo = zeros(1,length(x));
 R = zeros(1,length(x));
 V = zeros(1,length(x));
+Range = zeros(1,length(x));
 
 for i = 1 : length(x)
     R(i,1) = R_e(1) - R_p(i,1);
@@ -31,11 +32,18 @@ for i = 1 : length(x)
     V(i,1) = V_e(1) - V_p(i,1);
     V(i,2) = V_e(2) - V_p(i,2);
     V(i,3) = V_e(3) - V_p(i,3);
+    Range(i) = norm(R(i,:));
     Tgo(i) = tgo_interception(R(i,:),V(i,:),drho,m,minTgo);
 end
 
+for k = 1 : length(Range)
+    if (Range(k+1) > Range(k)) && (time_ref(k) - time_ref(1) > 10)
+        break
+    end
+end
+
 figure(1)
-plot(time_ref-time_ref(1),Tgo,'k','linewidth',1);
+plot(time_ref(1:k)-time_ref(1),Tgo(1:k),'k','linewidth',1);
 grid minor
 set(gca,'fontsize',16)
 set(gcf,'color','w')
